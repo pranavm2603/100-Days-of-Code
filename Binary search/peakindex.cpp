5e3676400b807b6bf624243750c999c33bbd53b8
Pranav Majagaonkar <96458920+pranavm2603@users.noreply.github.com>
2024-02-08 23:26:49 +0530

    Update peakindex.cpp

--- Binary search/peakindex.cpp
+++ Binary search/peakindex.cpp
@@ -3,19 +3,23 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
 
-        int left = 0, right = arr.size() - 1;
+        int s = 0;
+       int e = arr.size() - 1;
+       int mid = s + (e - s) / 2;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
+
+        while (s < e) {
+            
 
             if (arr[mid] < arr[mid + 1]) {
-                left = mid + 1;
+                s = mid + 1;
             } else {
-                right = mid;
+                e = mid;
             }
+            mid = s + (e - s) / 2;
         }
 
-        return left;
+        return s;
         
     }
 };
