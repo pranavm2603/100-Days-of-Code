c77006c0cbf3c60c494ccf24475d1f6008e0e877
Pranav Majagaonkar <96458920+pranavm2603@users.noreply.github.com>
2024-02-18 22:07:36 +0530

    Create Next Greater Element.cpp

---
+++ Next Greater Element.cpp
@@ -0,0 +1,31 @@
+Problem statement
+You are given an array 'a' of size 'n'.
+
+
+
+Print the Next Greater Element(NGE) for every element.
+
+
+
+The Next Greater Element for an element 'x' is the first element on the right side of 'x' in the array, which is greater than 'x'.
+
+
+
+If no greater elements exist to the right of 'x', consider the next greater element as -1.
+
+
+
+For example:
+Input: 'a' = [7, 12, 1, 20]
+
+Output: NGE = [12, 20, 20, -1]
+
+Explanation: For the given array,
+
+- The next greater element for 7 is 12.
+
+- The next greater element for 12 is 20. 
+
+- The next greater element for 1 is 20. 
+
+- There is no greater element for 20 on the right side. So we consider NGE as -1.
