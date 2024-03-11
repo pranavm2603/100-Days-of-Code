165c272d95267bf3ca8f3a0bceb6f7017ddade40
Pranav Majagaonkar <96458920+pranavm2603@users.noreply.github.com>
2024-03-11 18:52:02 +0530

    Create Check If The String Is A Palindrome.cpp

---
+++ Char Arrays and Strings/Check If The String Is A Palindrome.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h> 
+bool checkPalindrome(string s)
+{
+   int i=0;
+
+   int e=s.size()-1;
+
+    while(i<e)
+
+    {
+
+        while(i<e && !isalnum(s[i]))
+
+        {
+
+            i++;
+
+        }
+
+        while(i<e && !isalnum(s[e]))
+
+        {
+
+            e--;
+
+        }
+
+        int ans=s[i]-s[e];
+
+        if(ans==32 || ans==-32 || ans==0 )
+
+        {
+ 
+
+            i++;
+
+            e--;
+
+        }
+
+        else
+
+        {
+
+            return false;
+
+        }
+
+    }
+
+    return true;
+}
