6f11467064343d547b623fdf6bf50db024e89a74
Pranav Majagaonkar <96458920+pranavm2603@users.noreply.github.com>
2024-03-17 22:57:48 +0530

    Create Remove All Adjacent Duplicates In String.cpp

---
+++ Char Arrays and Strings/Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    std::string removeDuplicates(std::string s) {
+        int i = 0; // Index to track the position of the next character to be placed in the result string
+        
+        // Iterate through each character in the string
+        for (char c : s) {
+            if (i > 0 && s[i - 1] == c) {
+                // If the current character is equal to the previous character in the result string, skip it
+                // This effectively removes the adjacent duplicate characters
+                --i; // Move back one position in the result string
+            } else {
+                // Otherwise, place the current character in the result string
+                s[i++] = c;
+            }
+        }
+        
+        // The characters from index 0 to index i-1 in the string 's' represent the final result
+        return s.substr(0, i);
+    }
+};
