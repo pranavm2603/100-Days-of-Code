e700e811299fc9f6a8cef8d6b99ce5a87b1de903
Pranav Majagaonkar <96458920+pranavm2603@users.noreply.github.com>
2024-04-15 18:01:42 +0530

    Create Reverse Only Letters.cpp

---
+++ Char Arrays and Strings/Reverse Only Letters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            if (!isalpha(s[left])) {
+                left++;
+            } else if (!isalpha(s[right])) {
+                right--;
+            } else {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
+        }
+        
+        return s;
+    }
+};
